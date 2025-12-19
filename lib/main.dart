68d683c0279d9ebfeecfb2bf3a33219390937ca1
Mohamed Razeen <mohdrazeen.1011.06@gmail.com>
2025-12-19 20:37:37 +0530

    Added basic firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'pages/complaint_list_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -11,8 +20,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'EcoTrack',
       debugShowCheckedModeBanner: false,
       home: ComplaintListPage(),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('EcoTrack'),),
+      body: const Center(
+        child: Text('Firebase initialized...'),
+      ),
     );
   }
 }
